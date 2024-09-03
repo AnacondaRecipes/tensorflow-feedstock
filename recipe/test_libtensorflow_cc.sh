14b9aa83282348f1d2cc5f3746ffbe031425e17c
Charles Bousseau <cbousseau@anaconda.com>
2024-09-03 16:23:22 -0400

    osx-64 test configuration

--- recipe/test_libtensorflow_cc.sh
+++ recipe/test_libtensorflow_cc.sh
@@ -5,6 +5,9 @@ set -exuo pipefail
 if [[ "${target_platform}" == linux-* ]]; then
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
+if [[ "${target_platform}" == osx-64 ]]; then
+  export CXXFLAGS="${CXXFLAGS} -isysroot ${SDKROOT} -mmacosx-version-min=10.15"
+fi
 export CXXFLAGS="${CXXFLAGS} -std=c++17"
 export CXXFLAGS="${CXXFLAGS} -I${CONDA_PREFIX}/include/tensorflow/third_party"
 export CXXFLAGS="${CXXFLAGS} -I${CONDA_PREFIX}/include/tensorflow/third_party/xla"
