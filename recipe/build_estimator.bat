38d997ea06c014d3da76b196c506eef80a2a079b
Eric Lundby <Eric.Lundby@gmail.com>
2024-11-25 15:25:49 -0700

    Shutdown bazel after clean.

--- recipe/build_estimator.bat
+++ recipe/build_estimator.bat
@@ -9,4 +9,5 @@ bazel build tensorflow_estimator/tools/pip_package:build_pip_package
 bazel-bin\\tensorflow_estimator\\tools\\pip_package\\build_pip_package %WHEEL_DIR%
 python -m pip install --no-deps --no-build-isolation %WHEEL_DIR%\\*.whl
 bazel clean --expunge
+bazel shutdown
 popd
