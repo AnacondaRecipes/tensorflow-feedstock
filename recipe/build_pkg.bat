da327ac8b56d8516e01df678c4b57de0987c56a8
Eric Lundby <Eric.Lundby@gmail.com>
2025-04-25 08:19:15 -0600

    Basic windows support.

--- recipe/build_pkg.bat
+++ recipe/build_pkg.bat
@@ -1,5 +1,5 @@
 :: install the whl using pip
-pip install --no-deps %SRC_DIR%\\tensorflow_pkg\\*.whl
+%PYTHON% -m pip install --find-links=%SRC_DIR%\\tensorflow_pkg tensorflow_cpu --no-build-isolation --no-deps
 
 :: The tensorboard package has the proper entrypoint
 rm -f %LIBRARY_BIN%\\tensorboard
