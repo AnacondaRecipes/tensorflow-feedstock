b0fd20efc0f527b65e64e4bd974d91a723cae4c0
Eric Lundby <Eric.Lundby@gmail.com>
2025-10-03 14:57:06 -0600

    Remove unnecessary changes.

--- recipe/build_pkg.bat
+++ recipe/build_pkg.bat
@@ -3,6 +3,3 @@
 
 :: The tensorboard package has the proper entrypoint
 del /f /q "%LIBRARY_BIN%\tensorboard"
-
-:: These will come from the vendored protobuf headers output
-rmdir /s /q "%SP_DIR%\tensorflow\include\google\protobuf"
