bedad3108191cfa318ebf69c34a4c0de138bda1b
Eric Lundby <Eric.Lundby@gmail.com>
2026-03-13 15:35:39 -0600

    Use xxd shim same on unix.

--- recipe/build.sh
+++ recipe/build.sh
@@ -175,23 +175,7 @@ fi
 
 # XLA's embed_gpu_specs_gen genrule needs xxd, which isn't available as a
 # conda package. Provide a minimal Python shim that emulates `xxd -i`.
-cat > xxd <<'XXD_SCRIPT'
-#!/usr/bin/env python3
-import sys
-args = sys.argv[1:]
-if '-i' in args:
-    args.remove('-i')
-fname = args[0]
-with open(fname, 'rb') as f:
-    data = f.read()
-varname = fname.replace('/', '_').replace('.', '_').replace('-', '_')
-print(f"unsigned char {varname}[] = {{")
-for i in range(0, len(data), 12):
-    chunk = data[i:i+12]
-    print("  " + ", ".join(f"0x{b:02x}" for b in chunk) + ",")
-print("};")
-print(f"unsigned int {varname}_len = {len(data)};")
-XXD_SCRIPT
+cp "${RECIPE_DIR}/xxd.py" xxd
 chmod +x xxd
 
 source gen-bazel-toolchain
