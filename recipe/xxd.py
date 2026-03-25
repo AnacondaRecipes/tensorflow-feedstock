6cff991b2dd8128267121a3075d18e6ad1d9dd9a
Eric Lundby <Eric.Lundby@gmail.com>
2026-03-25 11:44:35 -0600

    add a comment to xxd.py

--- recipe/xxd.py
+++ recipe/xxd.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+"""Minimal Python shim for `xxd -i`, which is not available as a conda package.
+
+XLA's embed_files() genrule invokes `xxd -i` to convert binary files into C
+byte-array literals (see xla/util/build_defs.bzl, @xxd//:xxd). This script
+emulates that behavior so the Bazel build can find an xxd on PATH.
+
+Reference: https://github.com/openxla/xla/blob/37c3ee81054956406e4c0685c7d06a629de6fed0/xla/util/build_defs.bzl#L44
+"""
 import sys
 
 args = sys.argv[1:]
